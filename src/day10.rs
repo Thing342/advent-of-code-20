55079cfa59e42e20dc079bd721b1da9a6bcf62de
Wes Jordan <wjordan@mitre.org>
2020-12-10 20:28:19 -0500

    day 10, part 2

--- src/day10.rs
+++ src/day10.rs
@@ -11,11 +11,11 @@ pub fn main() {
     .collect::<Vec<u64>>();
 
   input.sort();
+  input.insert(0, 0);
+
+  //eprintln!("{:?}", &input);
 
   let mut deltas = [0, 0, 1];
-
-  let delta = input[0] as usize;
-  deltas[delta - 1] += 1;
 
   for i in 1..input.len() {
     let n0 = input[i-1];
@@ -25,7 +25,20 @@ pub fn main() {
     deltas[delta - 1] += 1;
   }
 
-  eprintln!("{:?}", deltas);
+  //eprintln!("{:?}", deltas);
 
   println!("DAY 10, PART 1: {}", deltas[0] * deltas[2]);
+
+  let p2: u64 =  input.windows(2)
+    .collect::<Vec<_>>()
+    .split(|a| a[1] - a[0] == 3) 
+    .map(|x| match x.len() {  
+        4 => 7,  
+        3 => 4,  
+        2 => 2,  
+        _ => 1  
+    })  
+    .product();
+
+  println!("DAY 10, PART 2: {}", p2);
 }
